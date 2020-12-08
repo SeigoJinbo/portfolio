2d99653863d4f36a2a36c76c400ccc9c56ce84fa
Seigo Jinbo <seigojinbo@gmail.com>
2020-12-08 11:08:36 +0900

    changed form image

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,24 +1,24 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const form = document.getElementById('contact-form');
+window.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("contact-form");
   // const button = document.getElementById("form-button");
-  const status = document.getElementById('status');
+  const status = document.getElementById("status");
 
   // Success and Error functions for after the form is submitted
 
   function success() {
     form.reset();
     // button.style = "display: none ";
-    status.classList.add('success');
-    status.innerHTML = 'Thanks!';
+    status.classList.add("success");
+    status.innerHTML = "Thanks!";
   }
 
   function error() {
-    status.innerHTML = 'Oops! There was a problem.';
+    status.innerHTML = "Oops! There was a problem.";
   }
 
   // handle the form submission event
 
-  form.addEventListener('submit', (ev) => {
+  form.addEventListener("submit", (ev) => {
     ev.preventDefault();
     const data = new FormData(form);
     ajax(form.method, form.action, data, success, error);
@@ -30,7 +30,7 @@ window.addEventListener('DOMContentLoaded', () => {
 function ajax(method, url, data, success, error) {
   const xhr = new XMLHttpRequest();
   xhr.open(method, url);
-  xhr.setRequestHeader('Accept', 'application/json');
+  xhr.setRequestHeader("Accept", "application/json");
   xhr.onreadystatechange = () => {
     if (xhr.readyState !== XMLHttpRequest.DONE) return;
     if (xhr.status === 200) {
